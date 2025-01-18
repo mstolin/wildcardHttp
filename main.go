a5b50bef9743d6cef1b16b8f39580dd0c3c9d771
Marcel Stolin <marcelstolin@gmail.com>
2025-01-18 17:57:22 +0100

    Updated to just print the request dump

--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
-type ResponseInfo struct {
-	URL    string              `json:"url"`
-	Path   string              `json:"path"`
-	Query  string              `json:"query"`
-	Method string              `json:"method"`
-	Body   []byte              `json:"body"`
-	Header map[string][]string `json:"header"`
-}
-
 func handleAny(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
 	dump, err := httputil.DumpRequest(r, true)
 	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprintf(w, "%e", err)
 		return
 	}
-
-	/*var resp ResponseInfo
-	resp.Method = r.Method
-	resp.URL = r.URL.String()
-	resp.Path = r.URL.Path
-	resp.Query = r.URL.RawQuery
-
-	// body
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return
-	}
-	resp.Body = body
-
-	// header
-	header := make(map[string][]string)
-	for key, value := range r.Header {
-		header[key] = value
-	}
-	resp.Header = header*/
-
-	json.NewEncoder(w).Encode(dump)
+	fmt.Fprintf(w, "%s", dump)
 }
 
 func main() {
